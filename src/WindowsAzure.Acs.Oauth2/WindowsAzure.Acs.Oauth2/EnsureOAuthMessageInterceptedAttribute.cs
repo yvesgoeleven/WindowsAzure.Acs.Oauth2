bdf592a73d6e5fa0f280bd8db492864707d22d00
Yves Goeleven <yves@goeleven.com>
2014-01-08 21:26:58 +0100

    another beautiful hack to make mvc behave like an api

--- src/WindowsAzure.Acs.Oauth2/WindowsAzure.Acs.Oauth2/EnsureOAuthMessageInterceptedAttribute.cs
+++ src/WindowsAzure.Acs.Oauth2/WindowsAzure.Acs.Oauth2/EnsureOAuthMessageInterceptedAttribute.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Net;
 using System.Web.Mvc;
+using Newtonsoft.Json;
 
 namespace WindowsAzure.Acs.Oauth2
 {
@@ -12,11 +15,27 @@ namespace WindowsAzure.Acs.Oauth2
 
         public override void OnAuthorization(AuthorizationContext filterContext)
         {
-            var authorizationServer = filterContext.Controller as AuthorizationServerBase;
-            if (authorizationServer != null)
+            try
             {
-                authorizationServer.StoreIncomingRequest(filterContext.HttpContext);
+                var authorizationServer = filterContext.Controller as AuthorizationServerBase;
+                if (authorizationServer != null)
+                {
+                    authorizationServer.StoreIncomingRequest(filterContext.HttpContext);
+                }
             }
+            catch (Exception ex)
+            {
+                var twolegged = filterContext.Controller as TwoLeggedAuthorizationServer;
+
+                if (twolegged != null)
+                {
+                    filterContext.HttpContext.Items["SuppressRedirect"] = HttpStatusCode.InternalServerError;
+                    filterContext.HttpContext.Items["SuppressRedirect.Result"] = JsonConvert.SerializeObject(new {message = ex.Message});
+                }
+
+                throw;
+            }
+            
         }
     }
 }
